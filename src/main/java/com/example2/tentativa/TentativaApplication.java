b6d17a7c53e89810cd05c927356b62f6a3d81af0
joaokax <lukaxsilva.ls@gmail.com>
2022-02-28 10:22:48 -0300

    Subindo a aplicação Spring Boot

---
+++ src/main/java/com/example2/tentativa/TentativaApplication.java
@@ -0,0 +1,13 @@
+package com.example2.tentativa;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TentativaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TentativaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example2/tentativa/TentativaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example2.tentativa;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TentativaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
